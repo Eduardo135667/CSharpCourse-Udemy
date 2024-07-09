81c43283f726ad01ff8f9f701074e9da0e9427f4
Eduardo135667 <el221006@gmail.com>
2024-07-08 21:13:54 -0300

    Heranca

---
+++ Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+            Bicicleta b = new Bicicleta();
+            
+        }
+    }
+}
